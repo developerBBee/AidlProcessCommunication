64d4a038ed103f847f079d7cab69f385a10c3bc2
BBee <atsushi.kokubun.47@gmail.com>
2022-04-15 23:48:27 +0900

    AidlProcessCommunication: getPackageName

--- app/src/main/java/jp/developer/bbee/aidlprocesscommunication/AIDLActivity.java
+++ app/src/main/java/jp/developer/bbee/aidlprocesscommunication/AIDLActivity.java
@@ -1,6 +1,8 @@
 package jp.developer.bbee.aidlprocesscommunication;
 
+import android.app.ActivityManager;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
@@ -8,22 +10,30 @@ import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AIDLActivity extends AppCompatActivity {
     private final String TAG = "AIDLActivity";
     private Intent mIntent;
     private IBookManager mIBookManager;
+    private ActivityManager mActivityManager;
+    private ListView listViewRecent;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aidl);
         mIntent = new Intent(AIDLActivity.this, BookManagerService.class);
+
+        listViewRecent = findViewById(R.id.listViewRecent);
+        mActivityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
     }
 
     public void onClick(View view) {
@@ -34,7 +44,23 @@ public class AIDLActivity extends AppCompatActivity {
             case R.id.unbindService:
                 unbindService(mConnection);
                 break;
+            case R.id.getRecent:
+                List<String> recentList = getRecent();
+                ArrayAdapter<String> adapter = new ArrayAdapter(
+                        AIDLActivity.this, android.R.layout.simple_list_item_1, recentList);
+                listViewRecent.setAdapter(adapter);
+                break;
         }
+    }
+
+    private List<String> getRecent() {
+        List<String> recentList = new ArrayList<>();
+        List<ActivityManager.RecentTaskInfo> taskList = mActivityManager.getRecentTasks(
+                256, ActivityManager.RECENT_WITH_EXCLUDED);
+        for (ActivityManager.RecentTaskInfo task : taskList) {
+            recentList.add(task.baseIntent.getComponent().getPackageName());
+        }
+        return recentList;
     }
 
     private ServiceConnection mConnection = new ServiceConnection() {
